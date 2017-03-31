7e3357d7eaef933f468b61bda804804c6ccf2282
amitiwary999 <tiwarya9@gmail.com>
2017-03-31 22:21:50 +0530

    updated apk

--- app/src/main/java/com/example/amit/remind_it/MainActivity.java
+++ app/src/main/java/com/example/amit/remind_it/MainActivity.java
@@ -181,9 +181,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /*Iif (id == R.id.action_settings) {
             return true;
-        }
+        }*/
 
         return super.onOptionsItemSelected(item);
     }
